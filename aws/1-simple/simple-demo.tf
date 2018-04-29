a45c6c7ede2ac5b9361e12bcc39d7b9b5901a462
Ganesh Pol <polganesh@gmail.com>
2018-04-29 16:36:15 +0530

    aws-1-simple-3-timeout

--- aws/1-simple/simple-demo.tf
+++ aws/1-simple/simple-demo.tf
@@ -9,10 +9,13 @@ provider "aws" {
 resource "aws_instance" "my-first-ec2" {
 	ami           = "ami-b46f48db"
   	instance_type = "t2.micro"
-  	
-  	  tags{
+  	tags{
   		Name="my-first-ec2-instance"
   		Project="demo-29-Apr-2018"
-  		}
-  	
+  	}
+  		
+  	timeouts {
+    	create = "30s"
+    	delete = "30s"
+  	}
 }
